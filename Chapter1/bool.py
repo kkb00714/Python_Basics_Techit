ea74fd0500f38de5e3fafd59ea4a408a9b51876f
kkb <0furiousblade0@gmail.com>
2023-09-08 18:07:21 +0900

    [#05 NEW] Python Bastics Chapter1 - 변수와 자료형(5)

---
+++ Chapter1/bool.py
@@ -0,0 +1,11 @@
+# 논리형(Bool, Boolean) / 참, 거짓을 나타내는 데이터
+# 대문자로 시작하는 키워드를 사용하여 씀
+
+# True
+# False
+
+print(3 > 2) # True
+print(3 == 2) # False / 보통 "==" 는 좌변과 우변의 값이 같다는 의미가 됨
+print(3 == 3.0) # True
+print(3 is 3.0) # False / "is" 는 자료형을 검증해볼 수 있음 
+
